60229ef277d7065997b5060efb7df6cb18a8dbe0
Miguel Martinez <miguel.martinez.angel.salazar@gmail.com>
2023-06-01 22:59:58 -0300

    ModificacionBalls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -12,24 +12,25 @@ const Ball = (props) => {
 
   return (
     <Float
-    speed={1.76} rotationIntensity={1} floatIntensity={2}
+    speed={1.75} rotationIntensity={1} floatIntensity={2}
     >
       <ambientLight intensity={0.9}/>
-      <directionalLight position={[0,0,0.05]}/>
+      <directionalLight position={[0, 0, 0.05]}/>
       <mesh 
       castShadow receiveShadow scale={2.75}
       >
-        <icosahedronGeometry args={[1,1]} />
+        <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
         color="#fff"
         polygonOffset
-        polygonOffsetFactor={5}
+        polygonOffsetFactor={-5}
         flatShading
         />
 
         <Decal
         position={[0,0,1]}
         rotation={[2 * Math.PI, 0, 6.25]}
+        scale={1}
         flatShading
         map={decal}
         />
