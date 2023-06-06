623ba53bb7b221760996ecc3e7a0ac0a5dda07c7
Miguel Martinez <miguel.martinez.angel.salazar@gmail.com>
2023-06-06 11:21:28 -0300

    MODIFICACION-CV

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -173,7 +173,7 @@ const Contact = () => {
           </div>
 
           <div className="justify-center items-center flex flex-col">
-          <a href="https://drive.google.com/file/d/1OQTSE7rHHhqLdi-ZrHqHVJgEX7SGDSG0/view?usp=sharing" target="_blanck">
+          <a href="https://drive.google.com/file/d/1MH4zbGxplGCCFhSszLwwWLrkdzgbfkYe/view?usp=sharing" target="_blanck">
             <img src={curriculum} alt="cv" className="w-16 h-16 hover:bg-[#4cf839] duration-300 rounded-lg p-1" />
           </a>
           <p>CV</p>
